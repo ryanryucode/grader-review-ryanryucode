e52c573be73fe6afca699b3416e53c424d52e02e
Ryan Ryu <132784680+ryanryucode@users.noreply.github.com>
2023-11-28 16:31:33 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -7,10 +7,24 @@ mkdir grading-area
 
 git clone $1 student-submission
 echo 'Finished cloning'
-
-
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
-
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+if [ -f "./student-submission/ListExamples.java" ]; 
+    then 
+        cp ./student-submission/ListExamples.java ./grading-area
+        cp ./TestListExamples.java ./grading-area
+        cp -r ./lib ./grading-area
+        cd grading-area
+        if ! javac -cp .:lib/hamcrest-core-1.3.jar:./lib/junit-4.13.2.jar *.java; then
+            echo "Compilation error"
+        else
+            set +e
+            java -cp .:lib/hamcrest-core-1.3.jar:./lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > ./result
+            if grep -q "OK" ./result; then
+                echo "Correct"
+            else
+                echo "Test case failed"
+            fi
+        fi
+       
+else 
+    echo "File does not exist or is in another directory"
+fi
